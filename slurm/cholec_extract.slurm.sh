ffe71d8bb1e8fe4124b62a25725f4bb67495f85e
federico@cunico.net <federico@cunico.net>
2025-09-17 10:46:08 +0200

    updated slurm

--- slurm/cholec_extract.slurm.sh
+++ slurm/cholec_extract.slurm.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1                       # node count
 #SBATCH --ntasks=12                     # total number of tasks across all nodes
 #SBATCH --mem=60G                       # total memory per node (4 GB per cpu-core is default)
-#SBATCH --gres=gpu:2                    # number of gpus per node
+#SBATCH --gres=gpu:4                    # number of gpus per node
 #SBATCH --time=25:00:00                 # total run time limit (HH:MM:SS)
 #SBATCH --mail-type=begin               # send mail when job begins
 #SBATCH --mail-type=end                 # send mail when job ends
@@ -18,9 +18,33 @@
 source $HOME/.bashrc
 
 module purge
-# module load anaconda3/2023.03
+module load anaconda3/2023.03
 module load profile/deeplrn
-# conda activate
-source ./venv/bin/activate
+module load autoload cudnn
 
+# conda activate
+conda activate aiprah
+
+
+##### Number of total processes 
+echo "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX "
+echo "Nodelist:= " $SLURM_JOB_NODELIST
+echo "Number of nodes:= " $SLURM_JOB_NUM_NODES
+echo "Ntasks per node:= "  $SLURM_NTASKS_PER_NODE
+echo "NUM gpu per done:=" $NUM_GPU
+echo "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX "
+
+echo "Run started at:- "
+date
+
+
+echo "###### starting ######"
+# torchrun --nnodes 1 --nproc_per_node $NUM_GPU --standalone run.py $flag1
 srun python datasets/cholec80.py
+
+echo ""
+echo "################################################################"
+echo "@@@@@@@@@@@@@@@@@@ Run completed at:- @@@@@@@@@@@@@@@@@@@@@@@@@"
+date
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo "################################################################"
